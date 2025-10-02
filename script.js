9f055dd91ee06ff5afe9da52c14b65b58e070a88
Megan202176 <megan202176@student-schoolsofwestfield.org>
2025-10-02 16:11:28 +0000

    changes

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
+/* Simple Guess Program */
+
+// define function guessGame
 function guessGame(){
+  // generate a random integer 1-100
   let answer = Math.floor(Math.random()*100)+1;
- alert(answer)
-  let guess=0;
-  while(guess!=answer){
-    guess = prompt("Guess a number, 1-100")
-    if(guess==answer) alert("Correct!")
-    else if(guess>answer) alert("Too high!")
-    else if(guess<answer) alert("Too low!")
-    else alert("Bad input")
+  // display the integer for testing purposes only
+  alert(answer)
+  // create guess and set equal to 0 
+  let guess=0
+  // loop while guess is not equal to answer or to "q"
+ while(guess!=answer){
+    // set guess equal to asking user to "Guess a number, 1-100"
+  guess=prompt("Guess a number 1-100!")
+    // if guess equals "q" display "Quitter!"
+  if(guess=="q") alert("Quitter!")
+    // or if guess equals answer display "Correct!" 
+  else if (guess==answer) alert("Correct!")
+    // or if guess is greater than answer display "Too high!" 
+  else if (guess>answer) alert("Too high!")
+    // or if guess is less than answer display "Too low!" 
+  else if (guess<answer) alert("Too low!")
+    // or say "Bad input"
+  else alert("Bad input")
+  // end the loop
   }
+// end the function
 }
