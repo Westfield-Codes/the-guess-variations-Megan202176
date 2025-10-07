425ad1a586f7f29d6a2f8917581505b45f79a322
Megan202176 <megan202176@student-schoolsofwestfield.org>
2025-10-07 15:55:22 +0000

    done

--- pseudocode/4-guess-play-again.js
+++ pseudocode/4-guess-play-again.js
@@ -51,3 +51,28 @@
     // end the loop
 
   // end the function
+function newGame(){
+  let again=true
+  let games=0
+while (again==true) {
+    games ++
+    if(games>1) {
+      again=confirm("Play again?")
+     }
+    guessGame();
+    }
+}
+
+function guessGame(){
+  let answer = Math.floor(Math.random()*100)+1;
+  alert(answer)
+  let guess=0
+  while(guess!=answer && guess!="q"){
+  guess=prompt("Guess a number 1-100!")
+  if(guess=="q") alert("Quitter!")
+  else if (guess==answer) alert("Correct!")
+  else if (guess>answer) alert("Too high!")
+  else if (guess<answer) alert("Too low!")
+  else alert("Bad input")
+  }
+}
