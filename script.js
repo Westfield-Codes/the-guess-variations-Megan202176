b7701baeb1203e7e3867b44c59d8a8126339a8d3
Megan202176 <megan202176@student-schoolsofwestfield.org>
2025-10-07 20:00:06 +0000

    guess loop quit

--- script.js
+++ script.js
@@ -1,74 +1,28 @@
-/* Guess Game Program */
-function newGame(){
-  let again=true
-  let games=0
-while (again==true) {
-    games ++
-    if(games>1) {
-      again=confirm("Play again?")
-     }
-    guessGame();
-    }
-}
+/* Simple Guess Program */
 
+// define function guessGame
 function guessGame(){
-  let answer = Math.floor(Math.random()*100)+1;
-  alert(answer)
-  let guess=0
-  while(guess!=answer && guess!="q"){
-  guess=prompt("Guess a number 1-100!")
-  if(guess=="q") alert("Quitter!")
-  else if (guess==answer) alert("Correct!")
-  else if (guess>answer) alert("Too high!")
-  else if (guess<answer) alert("Too low!")
-  else alert("Bad input")
-  }
-}
-// define function newGame
-function newGame(){
-  // create a Boolean variable for again, set to true
-  let again=true
-  // run the guessGame function
-
-  // loop as long as again is true 
-
-    // ask to play again with again set to a confirm
-
-    // if again is true run the guessGame function
-
-    // close the loop
-
-  // thank the player for playing
-
-  // close the function
-}
-
-// create a function called guessGame
-
-  // create guess and set equal to 0 
-
-  // create tries and set equal to 0 
-
-  // create variable answer and generate a random integer 1-100
-
+  // generate a random integer 1-100
+ let answer = Math.floor(Math.random()*100)+1;
   // display the integer for testing purposes only
-
-  // loop while guess is not equal to answer
-
+alert(answer)
+  // create guess and set equal to 0 
+let guess=0
+  // loop while guess is not equal to answer or to "q"
+  while(answer!=guess && answer!="q"){
     // set guess equal to asking user to "Guess a number, 1-100"
-
-    // add one to tries
-
-    // if guess equals answer display "Correct in [tries] tries!" 
-
-    // or if guess is "q" break the loop
-
+  guess= prompt("Guess a number 1-100.")
+    // if guess equals "q" display "Quitter!"
+  if( guess=="q") alert("Quitter!")
+    // or if guess equals answer display "Correct!" 
+  if( guess==answer) alert("Correct")
     // or if guess is greater than answer display "Too high!" 
-
+  if( guess>answer) alert("Too high")
     // or if guess is less than answer display "Too low!" 
-
-    // if it's none of those, say "Bad Input!"
-
-    // end the loop
-
-  // end the function
+  if( guess<answer) alert("Too Low")
+    // or say "Bad input"
+else alert("Bad Input")
+  // end the loop
+}
+// end the function
+}
