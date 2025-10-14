302fd5ac2fb1a9d55a70aceba9f78e30c4a6be49
Megan202176 <megan202176@student-schoolsofwestfield.org>
2025-10-14 20:23:25 +0000

    Guess Simple

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 /* Simple Guess Program */
 function guessGame(){
-// create variable called guess, set to 0
 let guess=0
-// create variable called answer and store a random integer 1-100
 let answer=Math.floor(Math.random()*100)+1;
-// display the answer for testing purposes only
 alert (answer)
 while(guess != answer) {
-// set guess equal to prompt asking user to "Guess a number, 1-100"
 guess= prompt("Guess a number 1-100")
-// if guess equals answer display "Correct!" 
-if (guess==answer) alert("Correct")
-// or if guess is greater than answer display "Too high!" 
+if (guess==answer) alert("Correct!") 
 else if (guess>answer) alert("Too high!")
-// or display "Too low!" 
 else alert("Too low!")
 }
 }
